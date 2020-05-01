44d18a77b0551c4c42ff22f64a74974aa3e4f545
AJU KARANAM <AJU KARANAM>
2020-05-01 08:38:04 -0400

    new

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye  There
+          I was Changed
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was Changed
+          Learn React
         </a>
       </header>
     </div>
